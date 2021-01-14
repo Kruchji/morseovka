de8b187b75c071bbf1a22a49775fdbad0a4cc984
Jiří Kruchina <kruchi@centrum.cz>
2021-01-14 09:23:33 +0100

    Přidání WOOO

--- morseovka.py
+++ morseovka.py
@@ -1,3 +1,3 @@
-print("Projekťák WOOOO WOOOO")
+print("Projekťák WOOOO WOOOO WOOOO")
 print("Zdravíčko elitní týme plný expertů na dané téma: Morseovka")
 print("Převodník, který by bez 3 lidí NEFUNGOVAL")
