a24f22d26f6e4fe9a66ac7eeb2e838e9e599cff7
Jiří Kruchina <kruchi@centrum.cz>
2021-01-15 18:55:13 +0100

    Test VSC

--- morseovka.py
+++ morseovka.py
@@ -1,4 +1,4 @@
 print("Projekťák WOOOO WOOOO WOOOO")
 print("Zdravíčko elitní týme plný expertů na dané téma: Morseovka")
 print("Převodník, který by bez 3 lidí NEFUNGOVAL")
-print("zz")
+a = [[9,8][1,5]]
