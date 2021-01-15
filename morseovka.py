f4f83227113526bfa7c307074673429f275ff204
Jiří Kruchina <kruchi@centrum.cz>
2021-01-15 19:08:39 +0100

    Added b

--- morseovka.py
+++ morseovka.py
@@ -2,3 +2,4 @@ print("Projekťák WOOOO WOOOO WOOOO")
 print("Zdravíčko elitní týme plný expertů na dané téma: Morseovka")
 print("Převodník, který by bez 3 lidí NEFUNGOVAL")
 a = [[9,8][1,5]]
+b = [[9,7][1,5]]
