1a6eff87b9081386df3df6632290cd7bde8b01ea
Jiří Kruchina <kruchi@centrum.cz>
2021-01-28 21:27:21 +0100

    Překlad do morseovštiny zprovozněn

--- morseovka.py
+++ morseovka.py
@@ -1,11 +1,15 @@
-def zmor(a):
-    if a in dictionary2:
-        #Vrátí překlad ze slovníku - easy peasy
-        return dictionary2[a]
+def mor(a):
+    if a in dictionary1 or a in dictionary2:
+        if vol == "do":
+            #Přeloží znak do morseovštiny - VERY HARD - a přídá custom oddělovač (weirdchamp pokud to není mezera)
+            return dictionary1[a] + sep
+        elif vol == "z":
+            #Vrátí překlad ze slovníku - easy peasy
+            return dictionary2[a]
     else:
         #Velice důležitá část kódu - co nejvíce se vysmát člověku za to, že neumí zadat morseovku
         exit("Neumíš morsevku LMAOOOO - špatný symbol: " + a)
-    
+        
 #Na následujících 4 řádcích celý kód stojí - v žádném případě NEODSTRAŇOVAT!!
 print("Projekťák WOOOO WOOOO WOOOO")
 print("Zdravíčko elitní týme plný expertů na dané téma: Morseovka")
@@ -15,7 +19,7 @@ print("zz")
 #WIP slovníky
 dictionary1 = {"A":".-", "B":"-...", "C":"-.-.","D":"-..", "E":".","F":"..-.","G":"--.","H":"....","I":"..","J":".---","K":"-.-","L":".-..","M":"--","N":"-.","O":"---","P":".--.","Q":"--.-","R":".-.","S":"...","T":"-","U":"..-","V":"...-","W":".--","X":"-..-","Y":"-.--","Z":"--..",
 "CH":"----","Ä":".-.-","Ë":"..–..","Ö":"---.","Ü":"..--","1":".----","2":"..---","3":"...--","4":"....-","5":".....","6":"-....","7":"--...","8":"---..","9":"----.","0":"-----",
-"?":"..--..",",":"--..--",".":".-.-.-",";":"-.-.-.","/":"-..-.","=":"-...-","-":"-....-","'":".----.","(":"-.--.",")":"-.--.-","\"":".-..-.",":":"---...","_":"..--.-","+":".-.-.","@":".--.-."," ":"/"," ":"","!":"-.-.--"
+"?":"..--..",",":"--..--",".":".-.-.-",";":"-.-.-.","/":"-..-.","=":"-...-","-":"-....-","'":".----.","(":"-.--.",")":"-.--.-","\"":".-..-.",":":"---...","_":"..--.-","+":".-.-.","@":".--.-."," ":"/","!":"-.-.--",
 "Á":".-","É":".","Ě":".","Í":"..","Ó":"---","Ú":"..-","Ů":"..-","Ý":"-.--","Č":"-.-.","Ř":".-.","Š":"...","Ž":"--..","Ť":"-"}
 
 dictionary2 = {".-":"A","-...":"B","-.-.":"C","-..":"D",".":"E","..-.":"F","--.":"G","....":"H","..":"I",".---":"J","-.-":"K",".-..":"L","--":"M","-.":"N","---":"O",".--.":"P","--.-":"Q",".-.":"R","...":"S","-":"T","..-":"U","...-":"V",".--":"W","-..-":"X","-.--":"Y","--..":"Z",
@@ -40,16 +44,21 @@ while True:
         text = raw.replace("–","-").replace("−","-").replace("_","-").replace("·",".").replace("*",".").split(sep)
         #Využijeme znalostí zeměpisu k rozluštění morseovky (Outstanding move right here!)
         #Prostě to na každé písmeno aplikuje funkci zmor, která písmeno v morseovce nahradí reálným písmenem
-        res = map(zmor, text)
-        final = ""
+        res = map(mor, text)
         #Lepší způsob na převedení listu do stringu jsem nenašel, takže here we go
-        print(final.join(res))
+        print("".join(res))
         break
 
     #PŘEKLAD DOMOR
     elif vol == "do":
-        raw = input("Vložte morseovku k přeložení: ")
-        raw = raw.upper()
+        raw = input("Vložte text k přeložení: ")
+        sep = input("Vložte oddělovač písmen: ")
+        #MILUJU CAPS-LOCK
+        text = raw.upper()
+        #TO VÍME, TO ZNÁME
+        res = map(mor, text)
+        #TO VÍME, TO ZNÁME
+        print("".join(res))
         break
 
     #TI CO CHTĚJÍ BÝT VÝJIMEČNÍ
