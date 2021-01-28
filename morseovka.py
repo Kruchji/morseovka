fce66bb419af7b234988564ed8e1dd9796acbefe
Jiří Kruchina <kruchi@centrum.cz>
2021-01-28 20:53:52 +0100

    Opakuj otázku než uživatele nedonutíš se podvolit

--- morseovka.py
+++ morseovka.py
@@ -23,26 +23,34 @@ dictionary2 = {".-":"A","-...":"B","-.-.":"C","-..":"D",".":"E","..-.":"F","--."
 
 #uživatele si necháme zvolit zda chce dekodovat či kodovat (nejsme jako ti, co vám ani nedají na výběr)
 print("Zvolte zda budete chtít překládat do moseovky/ z morseovky:")
-vol=input("Pro překlad do morseovky napiš: do, pro překlad z morseovky napiš: z ")
-#PŘEKLAD ZMOR
-if vol == "z":
-    #Pullneme klasický Facebook move, kdy z uživatelů vytáhneme všechna jejich data
-    raw = input("Vložte morseovku k přeložení: ")
-    print("Abychom vaší šifru mohli dekodovat musíte vložit oddělovač písmen. Oddělovč nesmí být: . - – − _ · *")
-    sep = input("Vložte oddělovač písmen: ")
-    #TO-DO: Upozornit uživatele, jaké oddělovače nemůže vložit (.-–−_·*)
-    #Tohle hopefully zajistí větší kompatibilitu s jinými překladači (a nakonec to rozdělí na písmena)
-    text = raw.replace("–","-").replace("−","-").replace("_","-").replace("·",".").replace("*",".").split(sep)
-    #Využijeme znalostí zeměpisu k rozluštění morseovky (Outstanding move right here!)
-    #Prostě to na každé písmeno aplikuje funkci zmor, která písmeno v morseovce nahradí reálným písmenem
-    res = map(zmor, text)
-    final = ""
-    #Lepší způsob na převedení listu do stringu jsem nenašel, takže here we go
-    print(final.join(res))
-#PŘEKLAD DOMOR
-elif vol == "do":
-    raw = input("Vložte morseovku k přeložení: ")
-    raw = raw.upper()
-#TI CO CHTĚJÍ BÝT VÝJIMEČNÍ
-else:
-    print("Zadali jste špatnou volbu!!!")
+
+#Tohle uživatele donutí strefit se na klávesnici
+while True:
+    vol=input("Pro překlad do morseovky napiš: 'do', pro překlad z morseovky napiš 'z': ")
+
+    #PŘEKLAD ZMOR
+    if vol == "z":
+        #Pullneme klasický Facebook move, kdy z uživatelů vytáhneme všechna jejich data
+        raw = input("Vložte morseovku k přeložení: ")
+        print("Abychom vaší šifru mohli dekodovat musíte vložit oddělovač písmen. Oddělovč nesmí být: . - – − _ · *")
+        sep = input("Vložte oddělovač písmen: ")
+        #TO-DO: Upozornit uživatele, jaké oddělovače nemůže vložit (.-–−_·*)
+        #Tohle hopefully zajistí větší kompatibilitu s jinými překladači (a nakonec to rozdělí na písmena)
+        text = raw.replace("–","-").replace("−","-").replace("_","-").replace("·",".").replace("*",".").split(sep)
+        #Využijeme znalostí zeměpisu k rozluštění morseovky (Outstanding move right here!)
+        #Prostě to na každé písmeno aplikuje funkci zmor, která písmeno v morseovce nahradí reálným písmenem
+        res = map(zmor, text)
+        final = ""
+        #Lepší způsob na převedení listu do stringu jsem nenašel, takže here we go
+        print(final.join(res))
+        break
+
+    #PŘEKLAD DOMOR
+    elif vol == "do":
+        raw = input("Vložte morseovku k přeložení: ")
+        raw = raw.upper()
+        break
+
+    #TI CO CHTĚJÍ BÝT VÝJIMEČNÍ
+    else:
+        print("Zadali jste špatnou volbu!!!")
